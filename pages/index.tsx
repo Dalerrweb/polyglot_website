59d5ccd9b5393dca5f05bb41ac01ed870810f5d9
Mirsaid25 <siddikovmirsaid01@gmail.com>
2023-09-15 20:45:17 +0500

    test page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,16 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Hero from "@/components/Hero";
+import Layout from "@/layout/Layout";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
    return (
-      <>
+      <Layout>
          <section>
             <Hero />
          </section>
-      </>
+      </Layout>
    );
 }
