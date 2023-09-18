c0f29c3d4e57bf3e4be536dbde7003c5486ce3ad
Mirsaid25 <siddikovmirsaid01@gmail.com>
2023-09-18 12:04:10 +0500

    fixed bug

--- pages/[testingPage].tsx
+++ pages/[testingPage].tsx
@@ -1,4 +1,4 @@
-import Timer from "../components/Timer";
+import Timer from "@/components/Timer";
 import { GetServerSideProps } from "next";
 import Image from "next/image";
 import Link from "next/link";
---
+++ pages/languages/eng/eng.ts
@@ -0,0 +1,92 @@
+const eng = {
+    header: {
+        catalog: "Catalog",
+        drivers: "For Drivers",
+        company: "About company",
+        orderBtn: "Order",
+        consult: 'Consultation'
+    },
+    banner: {
+        rentText: "Rent the best cars",
+        text: "Discover the world of Uzbekistan with us!",
+        text2: "Trips to all regions",
+        
+        text3: "Tours in the mountains",
+        text4: "Transfer to the airport or railway station",
+        text5: "Tourist tours and excursions to the ancient cities of Uzbekistan: Samarkand, Khiva, Bukhara and Shakhrisabz",
+        orderBtn: "Order",
+        infoBtn: "More",
+        mainText: "Discover the world of",
+        world: 'Uzbekistan',
+        us: 'with us'
+    },
+    cars: {
+        carsText: 'Our cars',
+        carsText2: "Choose whatever you like, our wide range of cars will always be to your liking!"
+    },
+    statistics: {
+        num: 'We are in numbers',
+        cars: 'Available number of cars',
+        rentedCars: 'Tourism services provided',
+        count: 'Number of counterparties'
+    },
+    questions: {
+        questionText: 'Frequent \n questions',
+        title: 'What are our services?',
+        title2: 'How do we provide car rental?',
+        titleText: 'Everything is very simple! Fill out a short form and within 12 hours we will contact you to provide detailed information',
+        questions: 'Do you have any more questions?',
+        questions2: 'Leave a request, and we will contact you to help you choose what interests you!'
+    },
+    modal: {
+        application: 'Submit your application',
+        placeholder: 'Your name',
+        btn: 'Send',
+        text: 'By clicking the "Submit" button, I give my consent to the processing and dissemination of personal data.',
+        phoneNumber: 'Phone number'
+    },
+    footer: {
+        menu: 'Меню',
+        company: 'Kompaniya',
+        rules: 'User Rules',
+        terms: 'Terms of use',
+        media: 'Social media'
+    },
+    catalogPage: {
+        mainText: 'Car catalog',
+        linkText: 'Main page / Catalog',
+        btn: 'Search',
+        typeOfCar: 'Сar type',
+        notFoundText: "We didn't find what you were looking for"
+    },
+    productPage: {
+        parametres: 'Parametrs',
+        driver: 'Driver',
+        yesText: 'Yes',
+        noText: 'No',
+        baggage: 'Baggage',
+        passenger: 'Passenger seats',
+        fuel: 'Fuel consumption',
+        price: 'Price',
+        characteristic: 'Characteristics',
+        mainText: 'Engine and transmission',
+        description: 'Description',
+        Engine: 'Engine',
+        count: 'Number, arrangement of cylinders',
+        text: 'Displacement (cc)',
+        text2: 'Maximum power (hp at rpm)',
+        text3: 'Maximum torque (Nm at rpm)',
+        text4: 'Transmission type',
+        successText: 'SUCCESS',
+        successText2: 'Expect our call within 24 hours'
+    },
+    errors: {
+        NameMsg: 'Name must be filled!',
+        NumMsg: 'Phone number must be filled!',
+    },
+    map: {
+        adress: 'Our adress - Uzbekistan street 41'
+    }
+}
+
+export default eng;
