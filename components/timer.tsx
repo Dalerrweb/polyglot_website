aaaf131940157b5023833dd1f06b2f4f2fce12c1
Mirsaid25 <siddikovmirsaid01@gmail.com>
2023-09-18 11:46:17 +0500

    nothing

--- components/timer.tsx
+++ components/timer.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 const Timer:any = ({time , setEndTest}:any) => {
     const timer = Date.now() + time;
