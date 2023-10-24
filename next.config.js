4c45343fb76814f651fae5888acef25bea27d124
Daler <dsarifkulo021@gmail.com>
2023-10-24 22:24:03 +0500

    fixed

--- next.config.js
+++ next.config.js
@@ -4,6 +4,10 @@ const nextConfig = {
    i18n: {
       locales: ["uz", "ru", "en"],
       defaultLocale: "en",
-   }
+   },
+   env: {
+      RECAPTCHA_SITE_KEY: '6LfXYcgoAAAAAG3HGb3BA8grJYblnb2MgcmaKVCM',
+      RECAPTCHA_SECRET_KEY: '6LfXYcgoAAAAAHwdFbLNMNzxhFsxVjHjGgJLD4jp',
+   },
 };
 module.exports = nextConfig;
