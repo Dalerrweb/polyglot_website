38e04cc3f25bc316e6ccf025f1b775a7c976b343
jony <sabinalatipova20@gmail.com>
2024-07-26 16:32:28 +0500

    test

--- components/FastMarquee.tsx
+++ components/FastMarquee.tsx
@@ -8,7 +8,7 @@ const FastMarquee = () => {
             <Marquee className='bg-blue pb-5 max-xl:pb-3'>
                 {
                     [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 28, 30, 32].map((i: number) => (
-                        <div key={i} className="px-3 max-xl:px-2 min-w-[380px] max-xl:w-[300px] max-md:w-[250px] max-sm:min-w-[200px] h-[250px] max-2xl:h-[200px] max-xl:h-[170px] max-lg:h-[150px] max-md:min-h-[120px]">
+                        <div key={i} className="px-3 max-xl:px-2 min-w-[380px] max-xl:min-w-[300px] max-md:w-[250px] max-sm:min-w-[200px] h-[250px] max-2xl:h-[200px] max-xl:h-[170px] max-lg:h-[150px] max-md:min-h-[120px]">
                             <Image
                                 className="w-full h-full object-cover rounded-2xl max-lg:rounded-xl max-md:rounded-lg bg-[#D9D9D9]"
                                 src={`/images/offices/photo-${i}.webp`}
@@ -24,7 +24,7 @@ const FastMarquee = () => {
             <Marquee direction='right' className='bg-blue'>
                 {
                     [11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 29, 31, 33].map((i: number) => (
-                        <div key={i} className="px-3 max-xl:px-2 min-w-[380px] max-xl:w-[300px] max-md:w-[250px] max-sm:min-w-[200px] h-[250px] max-2xl:h-[200px] max-xl:h-[170px] max-lg:h-[150px] max-md:min-h-[120px]">
+                        <div key={i} className="px-3 max-xl:px-2 min-w-[380px] max-xl:min-w-[300px] max-md:w-[250px] max-sm:min-w-[200px] h-[250px] max-2xl:h-[200px] max-xl:h-[170px] max-lg:h-[150px] max-md:min-h-[120px]">
                             <Image
                                 className="w-full h-full object-cover rounded-2xl max-lg:rounded-xl max-md:rounded-lg bg-[#D9D9D9]"
                                 src={`/images/offices/photo-${i}.webp`}
