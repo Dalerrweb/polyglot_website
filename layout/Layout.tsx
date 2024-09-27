a17076a8a5ba8d3a6c651a02663537b02090d05c
jony <sabinalatipova20@gmail.com>
2024-09-27 16:08:03 +0500

    added meta pixel

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -1,5 +1,7 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import Head from "next/head";
+import Script from "next/script";
 
 type Layout = {
    children: React.ReactNode;
@@ -11,6 +13,21 @@ const Layout = ({ children }: Layout) => {
          <Header />
          <main>{children}</main>
          <Footer />
+         <Script>
+            {`!function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod ?
+               n.callMethod.apply(n, arguments) : n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1580211606041469');
+            fbq('track', 'PageView');`}
+         </Script>
+         <noscript><img height="1" width="1" style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1580211606041469&ev=PageView&noscript=1"
+         /></noscript>
       </>
    );
 };
